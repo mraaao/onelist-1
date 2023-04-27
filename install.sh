5b17a927bc0cbff63efa6cd92a7ee25a2d782610
mraaao <39720454+mraaao@users.noreply.github.com>
2023-04-28 07:32:09 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,11 +8,9 @@ chmod +x onelist
 echo FROM scratch > Dockerfile
 echo ADD ./ / >> Dockerfile
 echo EXPOSE 5245 >> Dockerfile
-echo CMD [""] >> Dockerfile
-
+echo CMD [\"/onelist\",\"server\",\"--no-prefix\"] >> Dockerfile
 
 ./onelist -run config
 sed -i 's#UserEmail=xxxx.@qq.com#UserEmail=123456@qq.com#g' config.env
 sed -i 's#UserPassword=xxxxx#UserPassword=123456#g' config.env
-
 ./onelist -run server
