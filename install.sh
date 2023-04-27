30a66a2c99f0e19a58cc6a2697bd65d0a9acf1d1
mraaao <39720454+mraaao@users.noreply.github.com>
2023-04-28 03:49:19 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,6 @@
-rm alist-linux-musl-amd64.tar.gz
-wget https://github.com/alist-org/alist/releases/latest/download/alist-linux-musl-amd64.tar.gz
-tar -zxvf alist-linux-musl-amd64.tar.gz
-rm alist-linux-musl-amd64.tar.gz
-chmod +x alist
-
-echo FROM scratch > Dockerfile
-echo ADD ./ / >> Dockerfile
-echo EXPOSE 5244 >> Dockerfile
-echo CMD [\"/alist\",\"server\",\"--no-prefix\"] >> Dockerfile
-
-./alist admin
-./alist server
+docker pull msterzhang/onelist:latest
+mkdir /root/onelist/config
+docker run -d --name onelist -e PUID=0 -e PGID=0 -e TZ=Asia/Shanghai -p 5245:5245 -v /root/onelist/config:/config --add-host api.themoviedb.org:13.224.161.90 msterzhang/onelist:latest
+sed -i 's#UserEmail=xxxx.@qq.com#UserEmail=123456@qq.com#g' /root/onelist/configconfig.env
+sed -i 's#UserPassword=xxxxx#UserPassword=123456#g' /root/onelist/configconfig.env
+docker restart onelist
