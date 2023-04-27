8d3147f491f863b3ff3d665106c496f77c3409e4
mraaao <39720454+mraaao@users.noreply.github.com>
2023-04-28 07:02:01 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,16 @@
-sudo -i
-docker pull msterzhang/onelist:latest
-mkdir -p /root/onelist/config
-docker run -d --name onelist -e PUID=0 -e PGID=0 -e TZ=Asia/Shanghai -p 5245:5245 -v /root/onelist/config:/config --add-host api.themoviedb.org:13.224.161.90 msterzhang/onelist:latest
-sed -i 's#UserEmail=xxxx.@qq.com#UserEmail=123456@qq.com#g' /root/onelist/configconfig.env
-sed -i 's#UserPassword=xxxxx#UserPassword=123456#g' /root/onelist/configconfig.env
-docker restart onelist
+rm onelist-linux-musl-amd64.tar.gz
+wget https://github.com/msterzhang/onelist/releases/latest/download/onelist-linux-musl-amd64.tar.gz
+
+tar -zxvf onelist-linux-musl-amd64.tar.gz
+rm onelist-linux-musl-amd64.tar.gz
+chmod +x onelist
+
+./onelist -run config
+sed -i 's#UserEmail=xxxx.@qq.com#UserEmail=123456@qq.com#g' config.env
+sed -i 's#UserPassword=xxxxx#UserPassword=123456#g' config.env
+
+echo FROM scratch > Dockerfile
+echo ADD ./ / >> Dockerfile
+echo EXPOSE 5245 >> Dockerfile
+echo CMD [\"./onelist\",\"-run\",\"server\"] >> Dockerfile
+./onelist -run admin
