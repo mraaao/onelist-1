c4b59e5816d966f43f912df16c10132335071aa9
mraaao <39720454+mraaao@users.noreply.github.com>
2023-04-28 03:53:58 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+sudo -i
 docker pull msterzhang/onelist:latest
 mkdir /root/onelist/config
 docker run -d --name onelist -e PUID=0 -e PGID=0 -e TZ=Asia/Shanghai -p 5245:5245 -v /root/onelist/config:/config --add-host api.themoviedb.org:13.224.161.90 msterzhang/onelist:latest
